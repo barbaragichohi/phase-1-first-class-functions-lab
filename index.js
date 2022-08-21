428031a99e6833f20f5f7c047415ed538b1ca246
Barbara Gichohi <my.github.barbaragichohi77@gmail.com>
2022-08-21 23:58:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (arr){
+    return arr.slice(0,2)
+}
+
+// console.log (returnFirstTwoDrivers());
+
+const returnLastTwoDrivers = function (arr2){
+   let len = arr2.length
+
+    return arr2.slice(len-2,len)
+    //  return drivers3;
+}
+
+// console.log (returnLastTwoDrivers());
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+console.log (selectingDrivers[0]);
+console.log (selectingDrivers[1]);
+
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function (value) {
+        return fareMultiplier * value;
+}
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+  
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers)
+
+}
+
+// let len = arr.length
+
+//     return arr.slice(len-2,len)
+
+
+
+
+
+
